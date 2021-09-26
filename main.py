6fa8d9938e05346c143d10733873b93d6f7957d8
SONG <simpledeveloper.songs@gmail.com>
2021-09-26 19:49:47 +0900

    input text

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+stubs = []
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
+
+
+@app.post("/")
+def form_post(request: Request, stub: str = Form(...)):
+    result = stub
+    print(result)
+    stubs.append(result)
+
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
+
